8465a6ad87360084369945ae122141663de1252a
julian <julianeduardovillamizarpena_14021994@outlook.com>
2021-07-14 16:14:30 -0500

    Adding a Pangolin class

---
+++ Pangolin.java
@@ -0,0 +1,17 @@
+/**
+*Esta clase proporciona la implantación de un Pangolin,
+*El cual es un mamífero pequeño de Asia y de Africa parecido
+*A una alcachofa con para al revés. Extendido por una cola
+*A la vista de la cual nos pone a pone a pensar que, en efecto,
+*Lo ridiculo ya no mata
+*@author Julian Villamizar Peña
+*@version 1.0
+*/
+
+
+public class Pangolin{
+
+
+
+
+}
